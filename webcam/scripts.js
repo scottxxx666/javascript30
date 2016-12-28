6edec8d2b1f536f36c47bcd2da538eb6792fc6aa
scottxxx666 <scottxxx666@gmail.com>
2016-12-29 03:43:57 +0800

    Webcam v1.0

---
+++ webcam/scripts.js
@@ -0,0 +1,134 @@
+'use strict'
+
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snapSound = document.querySelector('.snap-sound');
+const snapBtn = document.querySelector('#take-picture');
+const filtBtn = document.querySelectorAll('.filt-btn');
+let filtVar = { redVideo: false, shadowVideo: false, splitVideo: false };
+
+const videoStream = function videoStreamer (stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+}
+
+const showVideo = function showVideoFunction () {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(videoStream)
+        .catch(e => console.log(e));
+}
+
+const showCanvas = function videoToCanvas (e) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    setInterval(drawCanvas, 160);
+}
+
+const drawCanvas = function drawCanvasFunction () {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    rangeFilt();
+
+    Object.keys(filtVar).forEach(filt => {
+        if (filtVar[filt]) {
+        }
+    });
+    
+    if (filtVar.splitVideo) {
+        splitVideo();
+    }
+
+    if (filtVar.redVideo) {
+        redVideo();
+    }
+
+    if (filtVar.shadowVideo) {
+        shadowVideo();
+    }
+  
+}
+
+const snapVideo = function snapVideoFun () {
+    const file = canvas.toDataURL();
+
+    snapSound.currentTime = 0;
+    snapSound.play();
+
+    const snapLink = document.createElement('a');
+    const img = document.createElement('img');
+
+    img.setAttribute('src', file);
+    snapLink.appendChild(img);
+    snapLink.setAttribute('href', file);
+    snapLink.setAttribute('download', 'snapshot');
+    strip.appendChild(snapLink);
+}
+
+const redVideo = function redVideoFunction () {
+    const newCtx = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i=0; i<newCtx.data.length; i+=4) {
+        newCtx.data[i] += 255;
+    }
+    ctx.putImageData(newCtx, 0, 0);
+}
+
+const shadowVideo = function shadowVideoFun () {
+    ctx.globalAlpha = 0.3;
+}
+
+const splitVideo = function splitVideoFun () {
+    const newCtx = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < newCtx.data.length; i+=4) {
+        newCtx.data[i - 150] = newCtx.data[i + 0];
+        newCtx.data[i + 150] = newCtx.data[i + 1];
+        newCtx.data[i + 100] = newCtx.data[i + 2];
+    }
+    ctx.putImageData(newCtx, 0, 0);
+}
+
+const rangeFilt = function rangeFilter () {
+    const inputs = document.querySelectorAll('input[type=range]');
+    let range = {};
+    inputs.forEach(input => {
+        const name = input.name;
+        range[name] = input.value;
+    });
+
+    const newCtx = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i=0; i<newCtx.data.length; i+=4) {
+
+        if (!isInRange(range, newCtx.data[i + 0], newCtx.data[i + 1], newCtx.data[i + 2], newCtx.data[i + 3])) {
+            continue;
+        }
+
+        newCtx.data[i + 3] = 0;
+    }
+    ctx.putImageData(newCtx, 0, 0);
+}
+
+const isInRange = function isInRangeFun (range, r, g, b, alpha) {
+    if ((r < range.rmin)||(r > range.rmax)) {
+        return false;
+    }
+
+    if ((g < range.gmin)||(g > range.gmax)) {
+        return false;
+    }
+
+    if ((b < range.bmin)||(b > range.bmax)) {
+        return false;
+    }
+
+    return true;
+}
+
+showVideo();
+
+video.addEventListener('canplay', showCanvas);
+snapBtn.addEventListener('click', snapVideo);
+filtBtn.forEach(btn => btn.addEventListener('click', e => {
+    const name = e.target.dataset.fun;
+    filtVar[name] = !filtVar[name];
+}));
